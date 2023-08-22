792ee94531ce841e982c04841078db0a4a0619f8
Sisko <100540300+SiskoWeb@users.noreply.github.com>
2023-08-22 16:03:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function run() {
+
+
+    let elements = document.getElementsByClassName("gwt-PopupPanel paywall-overlay menu-popup.active menu-popup-glass");
+
+    elements.remove();
+}
+
+window.onload = run
